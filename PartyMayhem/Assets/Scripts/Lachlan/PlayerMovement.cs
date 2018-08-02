682166d5017e4da78bfeea172aef7ee48cc88a31
Muz <blue.cat.blue@outlook.com>
2018-08-02 21:41:37 +1000

    Added Movement Stuff

--- PartyMayhem/Assets/Scripts/Lachlan/PlayerMovement.cs
+++ PartyMayhem/Assets/Scripts/Lachlan/PlayerMovement.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 200;
+    public float turnSpeed;
 
+    public AudioClip punchSound1;
+    public AudioClip punchSound2;
+
+    private void Update()
+    {
+        Vector3 newPosition = new Vector3(Input.GetAxis("P1 Horizontal"), Input.GetAxis("P1 Vertical"), 0.0f);
+
+        //Vector3 newPosition = new Vector3();
+        //newPosition.x = (Input.GetAxis("P1 Horizontal") * speed) * Time.deltaTime;
+        //newPosition.y = (Input.GetAxis("P1 Vertical") * speed) * Time.deltaTime;
+        //transform.Translate(newPosition);
+    }
 }
