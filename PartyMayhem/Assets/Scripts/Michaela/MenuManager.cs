763214758e1fb3b1a93ecb854283b50cc481576e
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-02 21:34:35 +1000

    Menus Scene Started

---
+++ PartyMayhem/Assets/Scripts/Michaela/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public void OnStartButtonPress()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnSettingsButtonPress()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void OnCreditsButtonPress()
+    {
+        Debug.Log("Credits");
+    }
+
+    public void OnQuitButtonPress()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
