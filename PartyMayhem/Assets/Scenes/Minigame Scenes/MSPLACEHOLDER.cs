814dfaf1b7bb798abca392ae85c9cb475725557a
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-03 17:44:47 +1000

    UI Testing Scene Added

--- PartyMayhem/Assets/Scenes/Minigame Scenes/MSPLACEHOLDER.cs
+++ PartyMayhem/Assets/Scenes/Minigame Scenes/MSPLACEHOLDER.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MSPLACEHOLDER : MonoBehaviour {
+public class MSPlaceHolder : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
