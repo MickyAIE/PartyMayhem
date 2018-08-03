8c137daa7759be5f7866a863764e7beae6bc3e6c
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-03 21:17:55 +1000

    Menu Changes

--- PartyMayhem/Assets/Scripts/Michaela/MenuManager.cs
+++ PartyMayhem/Assets/Scripts/Michaela/MenuManager.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour {
 
     public Animator anim;
+    public bool pressSpace = false;
+
+    public AudioSource music;
+    public float musicVolume;
 
     public void Start()
     {
         anim.SetBool("goToSettings", false);
+        anim.SetBool("goToCredits", false);
+        anim.SetBool("startScreen", true);
+
+        musicVolume = 1f;
     }
 
+    public void Update()
+    {
+        music.volume = musicVolume;
+        if (pressSpace == false)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("startScreen", false);
+                musicVolume = 0.3f;
+                pressSpace = true;
+            }
+        }
+    }
 
     public void OnStartButtonPress()
     {
@@ -26,11 +47,19 @@ public class MenuManager : MonoBehaviour {
     public void OnSettingsBackPress()
     {
         anim.SetBool("goToSettings", false);
+        Debug.Log("Back");
     }
 
     public void OnCreditsButtonPress()
     {
+        anim.SetBool("goToCredits", true);
         Debug.Log("Credits");
+    }
+
+    public void OnCreditsBackPress()
+    {
+        anim.SetBool("goToCredits", false);
+        Debug.Log("Back");
     }
 
     public void OnQuitButtonPress()
