6a6c397176266aaf121a4c14a95a04a21777bf0f
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-09 22:29:27 +1000

    UI Testing

---
+++ PartyMayhem/Assets/Scripts/Michaela/UITestScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITestScene : MonoBehaviour {
+
+    public Animator anim;
+    
+    public void OnTopPress()
+    {
+        anim.SetBool("goDown", true);
+    }
+
+    public void OnDownPress()
+    {
+        anim.SetBool("goDown", false);
+    }
+}
