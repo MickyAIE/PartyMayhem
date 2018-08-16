b88e7101beeedecd4a01b866877c7cb82e7fb1ef
Brandon <bj.mawson@bigpond.com>
2018-08-16 22:15:38 +1000

    Speed Boost powerup

---
+++ PartyMayhem/Assets/Scripts/Brandon M/PowerUpBehavior1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class PowerUpBehavior : MonoBehaviour {
+
+    public PowerupController controller;
+
+    [SerializeField]
+    private speedboostcode powerup;
+
+
+    private Transform transform_;
+	// Use this for initialization
+	private void Awake ()
+    {
+        transform_ = transform;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (other.gameObject.tag == "Player");
+        {
+            ActivatePowerup();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void ActivatePowerup()
+    {
+        //controller.AcrivatePowerup(powerup);
+    }
+
+    public void SetPowerup(PowerUpActions powerUp)
+    {
+        this.powerup = powerup;
+        gameObject.name = powerup.name;
+    }
+}*/
---
+++ PartyMayhem/Assets/Scripts/Brandon M/PowerupBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupBehavior : MonoBehaviour
+{
+    #region Attributes
+
+    #region Component References
+    
+    public PowerupController controller;
+
+    [SerializeField]
+    private Powerup powerup;
+
+    private Renderer renderer_;
+
+    private Transform transform_;
+
+    public Material PowerupMaterial
+    {
+        get { return renderer_.material; }
+        set { renderer_.material = value; }
+    }
+
+    #endregion
+
+    private int rotationPerSecond = 180;
+
+    #endregion
+
+    #region Monobehaviour API
+
+    private void Awake()
+    {
+        transform_ = transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    
+        if (collision.gameObject.tag == "Player")
+        {
+            ActivatePowerup();
+        }
+    }
+
+    #endregion
+
+    private void ActivatePowerup()
+    {
+        controller.ActivatePowerup(powerup);
+    }
+
+    public void SetPowerup(Powerup powerup)
+    {
+        this.powerup = powerup;
+        gameObject.name = powerup.name;
+    }
+}
