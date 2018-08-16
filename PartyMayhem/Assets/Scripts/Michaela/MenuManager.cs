0fe35e9497c80f8726967eb5c1b3689e2b1b136c
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-16 21:10:11 +1000

    Settings Menu

--- PartyMayhem/Assets/Scripts/Michaela/MenuManager.cs
+++ PartyMayhem/Assets/Scripts/Michaela/MenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
@@ -9,7 +10,6 @@ public class MenuManager : MonoBehaviour {
     public bool pressSpace = false;
 
     public AudioSource music;
-    public float musicVolume;
     public AudioSource click;
 
     public bool hoverBoard = false;
@@ -25,24 +25,19 @@ public class MenuManager : MonoBehaviour {
 
     public AudioMixer audioMixer;
 
-    public void SetVolume(float volume)
-    {
-        audioMixer.SetFloat("volume", volume);
-    }
+    Resolution[] resolutions;
 
-    public void SetQuality (int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
+    public Dropdown resolutionDropdown;
 
-    public void Awake()
+
+
+    public void Start()
     {
         anim.SetBool("goToSettings", false);
         anim.SetBool("goToCredits", false);
         anim.SetBool("goToModes", false);
         anim.SetBool("startScreen", true);
 
-        musicVolume = 1f;
 
         hoverBoard = false;
         hoverTournament = false;
@@ -52,17 +47,66 @@ public class MenuManager : MonoBehaviour {
         tournamentModeInfo.SetActive(false);
         freeplayModeInfo.SetActive(false);
         hoverGuideText.SetActive(false);
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+               resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = 1;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        Debug.Log("Quality Level: " + qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Is fullscreen: " + isFullscreen);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        Debug.Log("Resolution: " + resolution);
     }
 
     public void Update()
     {
-        music.volume = musicVolume;
         if (pressSpace == false)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 anim.SetBool("startScreen", false);
-                musicVolume = 0.3f;
                 pressSpace = true;
             }
         }
