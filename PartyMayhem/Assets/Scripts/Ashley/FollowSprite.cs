111b58a92ff0bc3706ba091111423f51d1bf3630
PotatoesBasket <PotatoesBasket@hotmail.com>
2018-08-19 12:14:20 +1000

    Balls!
    - Soccer ball and dodgeball sprites.
    - Teensy little script that makes a sprite follow another sprite without parenting, can be used for shadows below the balls so they don't rotate.
    - Renamed Drum scene to Item Test. The balls are in there. They're fun to kick around.
    - Removed gross drum sprites.

---
+++ PartyMayhem/Assets/Scripts/Ashley/FollowSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSprite : MonoBehaviour
+{
+    //Attaches sprite to other sprite's position without affecting its rotation.
+
+    public Transform sprite;
+
+    private void Update()
+    {
+        transform.position = sprite.transform.position;
+    }
+}
