d8debcfa3dce44fa7fed230034a04d1791ea6b27
Muz <blue.cat.blue@outlook.com>
2018-08-25 17:42:20 +1000

    Rhythm Blitz Added!

---
+++ PartyMayhem/Assets/Scripts/Lachlan/RhythmManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmManager : MonoBehaviour
+{
+    [HideInInspector] public float scoreAward;
+
+
+	void Start()
+    {
+		
+	}
+
+	void Update()
+    {
+        CalculateScore();
+	}
+
+    void CalculateScore()
+    {
+        scoreAward = Mathf.Sin(Time.time * 20);
+
+        Debug.Log(scoreAward.ToString());
+    }
+}
