53a9c1309d8e8a6818ad3b47e9e780b2de24e6c2
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-08-27 16:39:25 +1000

    [Dodgeball] Enemy can now aim, throw ball, and move

--- PartyMayhem/Assets/Scripts/Michaela/Enemy.cs
+++ PartyMayhem/Assets/Scripts/Michaela/Enemy.cs
@@ -2,59 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Timers
+{
+    public float waitToMoveTimer = 2;
+    public float waitToMoveStartTime = 2;
+
+    public float throwTimer = 2;
+    public float throwStartTime = 2;
+
+    public float waitTimer = 3;
+    public float waitStartTime = 3;
+
+    public float Movetimer = .5f;
+    public float moveStartTime = .5f;
+}
+
+[System.Serializable]
+public class Bools
+{
+    public bool hasThrownBall = false;
+    public bool hasSpawnedBall = false;
+
+    public bool hasMoved = false;
+    public bool hasPickedNumber = false;
+}
+
 public class Enemy : MonoBehaviour {
+
+    public Timers timers;
+    public Bools bools;
 
     public float rotateSpeed = 3f;
     public float ballSpeed = 6f;
 
+    public int randomNumber;
+
     public GameObject player;
     public GameObject ballPrefab;
     public GameObject ballSpawn;
-
-    public float timerToThrow = 2;
-    public float startTimeToThrow = 2;
-
-    public float timerToWait = 2;
-    public float startTimeToWait = 2;
-
-    public bool hasThrownBall = false;
-    public bool hasSpawnedBall = false;
-
     public GameObject ball;
+
+    private void Start()
+    {
+        bools.hasThrownBall = false;
+        bools.hasSpawnedBall = false;
+        bools.hasMoved = false;
+        bools.hasPickedNumber = false;
+    }
 
     private void Update()
     {
-        if (hasThrownBall == false)
+        if (bools.hasThrownBall == false)
         {
             TurnToTarget();
 
-            if (hasSpawnedBall == false)
+            bools.hasPickedNumber = false;
+            bools.hasMoved = false;
+
+            if (bools.hasSpawnedBall == false)
             {
                 ball = Instantiate(ballPrefab, ballSpawn.transform.position, ballSpawn.transform.rotation, gameObject.transform);
-                hasSpawnedBall = true;
+                bools.hasSpawnedBall = true;
             }
 
-            timerToThrow -= Time.deltaTime;
-            if(timerToThrow <= 0)
+            timers.throwTimer -= Time.deltaTime;
+            if(timers.throwTimer <= 0)
             {
-                hasThrownBall = true;
-
+                bools.hasThrownBall = true;
             }
         }
 
-        if (hasThrownBall == true)
+        if (bools.hasThrownBall == true)
         {
-            hasSpawnedBall = false;
+            bools.hasSpawnedBall = false;
             ThrowBall();
+            RandomMove();
+
+            timers.waitTimer -= Time.deltaTime;
             
-            timerToWait -= Time.deltaTime;
-            if(timerToWait <= 0)
+            if(timers.waitTimer <= 0)
             {
-                timerToThrow = startTimeToThrow;
-                timerToWait = startTimeToWait;
+                timers.throwTimer = timers.throwStartTime;
+                timers.waitTimer = timers.waitStartTime;
+                timers.waitToMoveTimer = timers.waitToMoveStartTime;
                 Destroy(ball);
 
-                hasThrownBall = false;
+                bools.hasThrownBall = false;
             }
         }
     }
@@ -70,13 +104,53 @@ public class Enemy : MonoBehaviour {
             );
 
         transform.up = -direction;
-
-
     }
 
     public void ThrowBall()
     {
         float rotation = gameObject.transform.rotation.z;
-        ball.transform.position += Vector3.left * Time.deltaTime * ballSpeed;
+        ball.transform.position += -transform.TransformDirection(Vector3.up) * Time.deltaTime * ballSpeed;
+    }
+
+    public void RandomMove()
+    {
+        timers.waitToMoveTimer -= Time.deltaTime;
+        if(timers.waitToMoveTimer <= 0)
+        {
+            if (bools.hasPickedNumber == false)
+            {
+                randomNumber = Random.Range(0, 2);
+                bools.hasPickedNumber = true;
+            }
+
+            if (bools.hasMoved == false)
+            {
+                timers.Movetimer -= Time.deltaTime;
+                if (randomNumber == 0)
+                {
+                    if (timers.Movetimer > 0)
+                    {
+                        gameObject.transform.position += Vector3.up * Time.deltaTime;
+                    }
+                    else
+                    {
+                        bools.hasMoved = true;
+                        timers.Movetimer = timers.moveStartTime;
+                    }
+                }
+                if (randomNumber == 1)
+                {
+                    if (timers.Movetimer > 0)
+                    {
+                        gameObject.transform.position += Vector3.down * Time.deltaTime;
+                    }
+                    else
+                    {
+                        bools.hasMoved = true;
+                        timers.Movetimer = timers.moveStartTime;
+                    }
+                }
+            }
+        }
     }
 }
