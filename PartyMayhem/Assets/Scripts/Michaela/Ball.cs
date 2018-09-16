3c32f74721396e9042e75628b26a8f08908c051b
PotatoesBasket <PotatoesBasket@hotmail.com>
2018-09-16 20:41:52 +1000

    Dodgeball fix

--- PartyMayhem/Assets/Scripts/Michaela/Ball.cs
+++ PartyMayhem/Assets/Scripts/Michaela/Ball.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    private Enemy parentEnemy;
+
+    private void Awake()
+    {
+        parentEnemy = transform.parent.GetComponent<Enemy>();
+    }
+
+    private void Update()
+    {
+        if (parentEnemy.bools.hasThrownBall == true)
+            transform.parent = null;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
