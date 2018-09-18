acf679ce1b780cf4848ccccb79dc3d49d06a267c
HarHarHarHarHar <40631613+HarHarHarHarHar@users.noreply.github.com>
2018-09-18 15:37:43 +1000

    Fixed Ice Patch issue and Added a current leader

--- PartyMayhem/Assets/Scripts/Harry/CheckPoints.cs
+++ PartyMayhem/Assets/Scripts/Harry/CheckPoints.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class CheckPoints : MonoBehaviour {
 
+    public string CurrentLeader;
+    
     void OnTriggerEnter2D(Collider2D CheckPointUpdate)
     {
         if (CheckPointUpdate.tag !="Player") //If a gameobject enteres the collision and doesnt have the player tag, do not continue
             return;
 
         if (transform == LapsCounter.NextCheckpoint[LapsCounter.currentCheckpoint].transform) //If this checkpoints transform is equal to NextCheckpoint's transform (which is always the descending through the Array containing all 4 checkpoints)
-        {            
+        {
+            CurrentLeader = CheckPointUpdate.gameObject.name;
+            Debug.Log(CurrentLeader);
             if (LapsCounter.currentCheckpoint + 1 < LapsCounter.NextCheckpoint.Length)//Check so the current checkpoint counter is less than the next checkpoint.
             {                 
                 if (LapsCounter.currentCheckpoint == 0)//Add to currentLap if currentCheckpoint is
                     LapsCounter.currentLap++; //Adds a 1 to the current lap. When the scene begins the Lap counter is at 0 because the players start just before the start line.
-                LapsCounter.currentCheckpoint++; //Changes the current checkpoint to the next checkpoint (kind of a confusing sentence but it just moves down the array)
+                LapsCounter.currentCheckpoint++; //Changes the current checkpoint to the next checkpoint (kind of a confusing sentence but it just moves down the array)               
             }
             else
             {
--- PartyMayhem/Assets/Scripts/Harry/Winner.cs
+++ PartyMayhem/Assets/Scripts/Harry/Winner.cs
@@ -14,10 +14,14 @@ public class Winner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.gameObject == RacingGameManager.Players[0].gameObject && RacingGameManager.Laps1 >= GameManager.gameLaps) {RacingGameManager.Player1Wins = true; Debug.Log("Player1Wins"); } else { return; }// if this gameobject is the gameobject that is listed
-        if (this.gameObject == RacingGameManager.Players[1].gameObject && RacingGameManager.Laps1 >= GameManager.gameLaps) {RacingGameManager.Player2Wins = true; Debug.Log("Player2Wins"); } else { return; }//as [1] in the players array & has past 3 laps then 
-        if (this.gameObject == RacingGameManager.Players[2].gameObject && RacingGameManager.Laps1 >= GameManager.gameLaps) {RacingGameManager.Player3Wins = true; Debug.Log("Player3Wins"); } else { return; }//set the correct players winning bool to true
-        if (this.gameObject == RacingGameManager.Players[3].gameObject && RacingGameManager.Laps1 >= GameManager.gameLaps) {RacingGameManager.Player4Wins = true; Debug.Log("Player4Wins"); } else { return; }// which configures the correct winners text on screen
+        /*Debug.Log();
+        Debug.Log(GameManager.profiles[1].name);
+        Debug.Log(GameManager.profiles[2].name);
+        Debug.Log(GameManager.profiles[3].name);*/
+        if (this.gameObject.name == RacingGameManager.Players[0].name && RacingGameManager.Laps1 >= GameManager.gameLaps) {RacingGameManager.Player1Wins = true; Debug.Log("Player1Wins"); } else { return; }// if this gameobject is the gameobject that is listed
+        if (this.gameObject.name == RacingGameManager.Players[1].name && RacingGameManager.Laps2 >= GameManager.gameLaps) {RacingGameManager.Player2Wins = true; Debug.Log("Player2Wins"); } else { return; }//as [1] in the players array & has past 3 laps then 
+        if (this.gameObject.name == RacingGameManager.Players[2].name && RacingGameManager.Laps3 >= GameManager.gameLaps) {RacingGameManager.Player3Wins = true; Debug.Log("Player3Wins"); } else { return; }//set the correct players winning bool to true
+        if (this.gameObject.name == RacingGameManager.Players[3].name && RacingGameManager.Laps4 >= GameManager.gameLaps) {RacingGameManager.Player4Wins = true; Debug.Log("Player4Wins"); } else { return; }// which configures the correct winners text on screen
 
     }
 }
