585ead70361b377c627f42c1e4090e36bb15083d
Michaela-T <41561709+Michaela-T@users.noreply.github.com>
2018-09-19 17:42:56 +1000

    [Menus] Still working on Tournament

--- PartyMayhem/Assets/Scripts/Michaela/DodgeballPlayerExtra.cs
+++ PartyMayhem/Assets/Scripts/Michaela/DodgeballPlayerExtra.cs
@@ -11,26 +11,6 @@ public class DodgeballPlayerExtra : MonoBehaviour {
         dM = GameObject.FindGameObjectWithTag("MinigameManager").GetComponent<DodgeballManager>();
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Ball" && gameObject.name == "Player")
-        {
-            dM.playerOneHasBeenHit = true;
-        }
-        if (collision.gameObject.tag == "Ball" && gameObject.name == "Target1")
-        {
-            dM.playerTwoHasBeenHit = true;
-        }
-        if (collision.gameObject.tag == "Ball" && gameObject.name == "Target2")
-        {
-            dM.playerThreeHasBeenHit = true;
-        }
-        if (collision.gameObject.tag == "Ball" && gameObject.name == "Target3")
-        {
-            dM.playerFourHasBeenHit = true;
-        }
-    }*/
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ball" && gameObject.name == "Player1")
