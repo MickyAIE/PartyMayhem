e7cf9b8b1bf280f79a4c1264f275c8ac377e2cad
Brandon <bj.mawson@bigpond.com>
2018-09-20 21:08:47 +1000

    geochase update

--- PartyMayhem/Assets/Scripts/Brandon M/colouredtilepickup.cs
+++ PartyMayhem/Assets/Scripts/Brandon M/colouredtilepickup.cs
@@ -2,35 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class colouredtilepickup : MonoBehaviour {
-    /*GameObject p1;
-    GameObject p2;
-    GameObject p3;
-    GameObject p4;*/
+public class colouredtilepickup : MonoBehaviour
+{
+    public string whatPLAYER;
 
-    private void OnTriggerEnter2D(Collider2D colourchangercheck)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (colourchangercheck.name == "player1")
-            Destroy(gameObject);
+        if (other.name == "player1" && whatPLAYER == "1")
+        { Destroy(gameObject); }
+
+
+        if (other.name == "player2" && whatPLAYER == "1")
+        { Destroy(gameObject); }
+
+        if (other.name == "player3" && whatPLAYER == "1")
+        { Destroy(gameObject); }
+
+        if (other.name == "player4" && whatPLAYER == "1")
+        { Destroy(gameObject); }
     }
 
-    /*private void OnTriggerEnter2D(Collider2D p2)
+
+
+    GameObject Tile()
     {
-        if (p2.name == "player2")
-            Destroy(gameObject);
+        GameObject[] tile;
+
+        tile = GameObject.FindGameObjectsWithTag("tile");
+        GameObject tile => 1;
+        
+        foreach (GameObject tile)
+        {
+            if (tile < 1)
+            {
+               Debug.Log "Player" + /*what player +*/ "has won the game";
+            }
+        }
+        return tile;
     }
-
-    private void OnTriggerEnter2D(Collider2D p3)
-    {
-        if (p3.name == "player3")
-            Destroy(gameObject);
-    }
-
-    private void OnTriggerEnter2D(Collider2D p4)
-    {
-        if (p4.name == "player4")
-            Destroy(gameObject);
-    }*/
-
-
 }
