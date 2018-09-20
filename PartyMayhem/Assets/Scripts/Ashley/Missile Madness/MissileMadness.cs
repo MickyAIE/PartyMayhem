73ef535fe61e86cb61eab9227ddc6218d6b937af
PotatoesBasket <PotatoesBasket@hotmail.com>
2018-09-20 14:55:27 +1000

    HUD code added to Missile Madness

--- PartyMayhem/Assets/Scripts/Ashley/Missile Madness/MissileMadness.cs
+++ PartyMayhem/Assets/Scripts/Ashley/Missile Madness/MissileMadness.cs
@@ -9,8 +9,17 @@ public class MissileMadness : MonoBehaviour
     private GameManager manager;
 
     public GameObject[] players;
-    public Text timer;
     public Text message;
+    public Text timer;
+    public Slider timerBar;
+
+    public GameObject playerInfoPrefab;
+    public GameObject[] playerInfoPositions;
+    private GameObject player1Info;
+    private GameObject player2Info;
+    private GameObject player3Info;
+    private GameObject player4Info;
+    public Component[] playerProfiles;
 
     public AudioSource music;
     public AudioSource itemSound;
@@ -41,6 +50,7 @@ public class MissileMadness : MonoBehaviour
         manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         timeLimit = manager.gameTimer;
         timer.gameObject.SetActive(false);
+        timerBar.gameObject.SetActive(false);
         message.gameObject.SetActive(true);
         countdown = 3.5f;
 
@@ -56,6 +66,7 @@ public class MissileMadness : MonoBehaviour
     {
         manager.SpawnPlayers();
         players = GameObject.FindGameObjectsWithTag("Player");
+        playerProfiles = manager.GetComponents<PlayerProfile>();
 
         if (CurrentPlayerCount() == 1)
         {
@@ -63,11 +74,15 @@ public class MissileMadness : MonoBehaviour
         }
 
         DisablePlayerMovement();
+
+        timerBar.maxValue = timeLimit;
+        HUDSpawn();
     }
 
     private void Update()
     {
         CurrentPlayerCount();
+        PlayerInfos();
 
         switch (State)
         {
@@ -78,6 +93,7 @@ public class MissileMadness : MonoBehaviour
                 if (countdown <= 1)
                 {
                     timer.gameObject.SetActive(true);
+                    timerBar.gameObject.SetActive(true);
                     message.gameObject.SetActive(false);
 
                     EnablePlayerMovement();
@@ -89,11 +105,13 @@ public class MissileMadness : MonoBehaviour
                 timeLimit -= Time.deltaTime;
                 int seconds = Mathf.RoundToInt(timeLimit);
                 timer.text = string.Format("{0:D2}:{1:D2}", (seconds / 60), (seconds % 60));
+                timerBar.value = timeLimit;
 
                 if (timeLimit <= 0 || (CurrentPlayerCount() == 1 && !onePlayerMode) || CurrentPlayerCount() == 0)
                 {
                     message.text = "FINISH!";
                     timer.gameObject.SetActive(false);
+                    timerBar.gameObject.SetActive(false);
                     message.gameObject.SetActive(true);
                     countdown = 2f;
 
@@ -109,7 +127,7 @@ public class MissileMadness : MonoBehaviour
                 {
                     if (PlayerPrefs.GetInt("Mode") == 2)
                         manager.currentRound++;
-                        
+
                     if (PlayerPrefs.GetInt("Mode") == 2 && manager.currentRound == manager.rounds)
                         manager.returningToMenus = false;
                     else
@@ -179,6 +197,101 @@ public class MissileMadness : MonoBehaviour
             {
                 cmt.isPaused = false;
                 cmt.ResetAnimation();
+            }
+        }
+    }
+
+    private void HUDSpawn()
+    {
+        if (manager.ActivePlayerCount() == 1)
+        {
+            player1Info = Instantiate(playerInfoPrefab, playerInfoPositions[0].transform.position, Quaternion.identity, playerInfoPositions[0].transform);
+            player1Info.transform.GetChild(3).GetComponent<Text>().text = "Player 1";
+        }
+        else if (manager.ActivePlayerCount() == 2)
+        {
+            player1Info = Instantiate(playerInfoPrefab, playerInfoPositions[0].transform.position, Quaternion.identity, playerInfoPositions[0].transform);
+            player1Info.transform.GetChild(3).GetComponent<Text>().text = "Player 1";
+            player2Info = Instantiate(playerInfoPrefab, playerInfoPositions[1].transform.position, Quaternion.identity, playerInfoPositions[1].transform);
+            player2Info.transform.GetChild(3).GetComponent<Text>().text = "Player 2";
+        }
+        else if (manager.ActivePlayerCount() == 3)
+        {
+            player1Info = Instantiate(playerInfoPrefab, playerInfoPositions[0].transform.position, Quaternion.identity, playerInfoPositions[0].transform);
+            player1Info.transform.GetChild(3).GetComponent<Text>().text = "Player 1";
+            player2Info = Instantiate(playerInfoPrefab, playerInfoPositions[1].transform.position, Quaternion.identity, playerInfoPositions[1].transform);
+            player2Info.transform.GetChild(3).GetComponent<Text>().text = "Player 2";
+            player3Info = Instantiate(playerInfoPrefab, playerInfoPositions[2].transform.position, Quaternion.identity, playerInfoPositions[2].transform);
+            player3Info.transform.GetChild(3).GetComponent<Text>().text = "Player 3";
+        }
+        else if (manager.ActivePlayerCount() == 4)
+        {
+            player1Info = Instantiate(playerInfoPrefab, playerInfoPositions[0].transform.position, Quaternion.identity, playerInfoPositions[0].transform);
+            player1Info.transform.GetChild(3).GetComponent<Text>().text = "Player 1";
+            player2Info = Instantiate(playerInfoPrefab, playerInfoPositions[1].transform.position, Quaternion.identity, playerInfoPositions[1].transform);
+            player2Info.transform.GetChild(3).GetComponent<Text>().text = "Player 2";
+            player3Info = Instantiate(playerInfoPrefab, playerInfoPositions[2].transform.position, Quaternion.identity, playerInfoPositions[2].transform);
+            player3Info.transform.GetChild(3).GetComponent<Text>().text = "Player 3";
+            player4Info = Instantiate(playerInfoPrefab, playerInfoPositions[3].transform.position, Quaternion.identity, playerInfoPositions[3].transform);
+            player4Info.transform.GetChild(3).GetComponent<Text>().text = "Player 4";
+        }
+    }
+
+    public void PlayerInfos()
+    {
+        if (PlayerPrefs.GetInt("Mode") == 2)
+        {
+            if (player1Info != null)
+                player1Info.transform.GetChild(2).GetComponent<Text>().text = manager.player1Score.ToString();
+            if (player2Info != null)
+                player2Info.transform.GetChild(2).GetComponent<Text>().text = manager.player2Score.ToString();
+            if (player3Info != null)
+                player3Info.transform.GetChild(2).GetComponent<Text>().text = manager.player3Score.ToString();
+            if (player4Info != null)
+                player4Info.transform.GetChild(2).GetComponent<Text>().text = manager.player4Score.ToString();
+        }
+        else
+        {
+            if (player1Info != null)
+                player1Info.transform.GetChild(2).gameObject.SetActive(false);
+            if (player2Info != null)
+                player2Info.transform.GetChild(2).gameObject.SetActive(false);
+            if (player3Info != null)
+                player3Info.transform.GetChild(2).gameObject.SetActive(false);
+            if (player4Info != null)
+                player4Info.transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        if (player1Info != null)
+        {
+            foreach (PlayerProfile pp in playerProfiles)
+            {
+                if (pp.playerNumber == 1)
+                    player1Info.transform.GetChild(1).GetComponent<Image>().sprite = pp.playerPortrait;
+            }
+        }
+        if (player2Info != null)
+        {
+            foreach (PlayerProfile pp in playerProfiles)
+            {
+                if (pp.playerNumber == 2)
+                    player2Info.transform.GetChild(1).GetComponent<Image>().sprite = pp.playerPortrait;
+            }
+        }
+        if (player3Info != null)
+        {
+            foreach (PlayerProfile pp in playerProfiles)
+            {
+                if (pp.playerNumber == 3)
+                    player3Info.transform.GetChild(1).GetComponent<Image>().sprite = pp.playerPortrait;
+            }
+        }
+        if (player4Info != null)
+        {
+            foreach (PlayerProfile pp in playerProfiles)
+            {
+                if (pp.playerNumber == 4)
+                    player4Info.transform.GetChild(1).GetComponent<Image>().sprite = pp.playerPortrait;
             }
         }
     }
